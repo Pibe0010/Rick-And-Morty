a3a992e71b6618ef2108ac87c77c9547db18f695
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-09 22:41:02 +0200

    poniendo mas datos en las tarjetas

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -9,13 +9,12 @@ export const Character = ({ character }) => {
         </div>
         <div className="card_content">
           <span className="card_title">{character.name}</span>
-          <span className="card_subtitle">
-            Thsi is a subtitle of this page. Let us see how it looks on the Web.
-          </span>
+          <span className="card_subtitle">{character.species}</span>
           <p className="card_description">
-            Lorem ipsum dolor, sit amet expedita exercitationem recusandae aut
-            dolor tempora aperiam itaque possimus at, cupiditate earum, quae
-            repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            <p> Status :{character.status}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Origin: {character.origin.name}</p>
+            <p> Created: {character.created}</p>
           </p>
         </div>
       </article>
